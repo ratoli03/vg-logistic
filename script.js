46c072cb39e95ae07588af1cd68467a3c2cb6373
ratoli03 <jacekratajczyk1@gmail.com>
2026-07-05 03:08:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const navbar = document.querySelector('.navbar');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 40) {
-    navbar.style.background = 'rgba(8, 8, 8, 0.96)';
-  } else {
-    navbar.style.background = 'rgba(8, 8, 8, 0.82)';
-  }
-});
+// Script strony nie jest wymagany w tej wersji.
